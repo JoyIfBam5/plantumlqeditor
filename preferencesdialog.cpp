b0c3c4c9ffef6482d8e06cc86b3e9009cfd30def
Ionutz Borcoman <iborco@gmail.com>
2012-08-14 16:40:09 +0300

    refs #184 - more settings

--- preferencesdialog.cpp
+++ preferencesdialog.cpp
@@ -7,6 +7,7 @@
 
 namespace {
 const int TIMEOUT_SCALE = 1000;
+const int CACHE_SCALE = 1000;
 }
 
 PreferencesDialog::PreferencesDialog(FileCache* file_cache, QWidget *parent)
@@ -30,20 +31,34 @@ void PreferencesDialog::readSettings()
 
     settings.beginGroup(SETTINGS_MAIN_SECTION);
 
-    m_ui->customJavaPathEdit->setText(settings.value(SETTINGS_CUSTOM_JAVA_PATH).toString());
-    m_ui->customPlantUmlEdit->setText(settings.value(SETTINGS_CUSTOM_PLANTUML_PATH).toString());
-    m_ui->autoRefreshSpin->setValue(settings.value(SETTINGS_AUTOREFRESH_TIMEOUT).toInt() / TIMEOUT_SCALE);
-    m_ui->assistantXmlEdit->setText(settings.value(SETTINGS_ASSISTANT_XML_PATH).toString());
-
     if (settings.value(SETTINGS_USE_CUSTOM_JAVA, SETTINGS_USE_CUSTOM_JAVA_DEFAULT).toBool())
         m_ui->customJavaRadio->setChecked(true);
     else
         m_ui->defaultJavaRadio->setChecked(true);
+    m_ui->customJavaPathEdit->setText(settings.value(SETTINGS_CUSTOM_JAVA_PATH, SETTINGS_CUSTOM_JAVA_PATH_DEFAULT).toString());
 
     if (settings.value(SETTINGS_USE_CUSTOM_PLANTUML, SETTINGS_USE_CUSTOM_PLANTUML_DEFAULT).toBool())
         m_ui->customPlantUmlRadio->setChecked(true);
     else
         m_ui->defaultPlatUmlRadio->setChecked(true);
+    m_ui->customPlantUmlEdit->setText(settings.value(SETTINGS_CUSTOM_PLANTUML_PATH, SETTINGS_CUSTOM_PLANTUML_PATH_DEFAULT).toString());
+
+    if (settings.value(SETTINGS_USE_CUSTOM_GRAPHIZ, SETTINGS_USE_CUSTOM_GRAPHIZ_DEFAULT).toBool())
+        m_ui->customGraphizRadio->setChecked(true);
+    else
+        m_ui->defaultGraphizRadio->setChecked(true);
+    m_ui->customGraphizEdit->setText(settings.value(SETTINGS_CUSTOM_GRAPHIZ_PATH).toString());
+
+    m_ui->autoRefreshSpin->setValue(settings.value(SETTINGS_AUTOREFRESH_TIMEOUT).toInt() / TIMEOUT_SCALE);
+    m_ui->assistantXmlEdit->setText(settings.value(SETTINGS_ASSISTANT_XML_PATH).toString());
+
+    m_ui->cacheGroupBox->setChecked(settings.value(SETTINGS_USE_CACHE, SETTINGS_USE_CACHE_DEFAULT).toBool());
+    if (settings.value(SETTINGS_USE_CUSTOM_CACHE, SETTINGS_USE_CUSTOM_CACHE_DEFAULT).toBool())
+        m_ui->customCacheRadio->setChecked(true);
+    else
+        m_ui->defaultCacheRadio->setChecked(true);
+    m_ui->customCacheEdit->setText(settings.value(SETTINGS_CUSTOM_CACHE_PATH).toString());
+    m_ui->cacheMaxSize->setValue(settings.value(SETTINGS_CACHE_MAX_SIZE, SETTINGS_CACHE_MAX_SIZE_DEFAULT).toInt() / CACHE_SCALE);
 
     settings.endGroup();
 
@@ -58,12 +73,23 @@ void PreferencesDialog::writeSettings()
 
     settings.beginGroup(SETTINGS_MAIN_SECTION);
 
+    settings.setValue(SETTINGS_USE_CUSTOM_JAVA, m_ui->customJavaRadio->isChecked());
     settings.setValue(SETTINGS_CUSTOM_JAVA_PATH, m_ui->customJavaPathEdit->text());
+
+    settings.setValue(SETTINGS_USE_CUSTOM_PLANTUML, m_ui->customPlantUmlRadio->isChecked());
     settings.setValue(SETTINGS_CUSTOM_PLANTUML_PATH, m_ui->customPlantUmlEdit->text());
+
+    settings.setValue(SETTINGS_USE_CUSTOM_GRAPHIZ, m_ui->customGraphizRadio->isChecked());
+    settings.setValue(SETTINGS_CUSTOM_GRAPHIZ_PATH, m_ui->customGraphizEdit->text());
+
     settings.setValue(SETTINGS_AUTOREFRESH_TIMEOUT, m_ui->autoRefreshSpin->value() * TIMEOUT_SCALE);
     settings.setValue(SETTINGS_ASSISTANT_XML_PATH, m_ui->assistantXmlEdit->text());
-    settings.setValue(SETTINGS_USE_CUSTOM_JAVA, m_ui->customJavaRadio->isChecked());
-    settings.setValue(SETTINGS_USE_CUSTOM_PLANTUML, m_ui->customPlantUmlRadio->isChecked());
+
+    settings.setValue(SETTINGS_USE_CACHE, m_ui->cacheGroupBox->isChecked());
+    settings.setValue(SETTINGS_USE_CUSTOM_CACHE, m_ui->customCacheRadio->isChecked());
+    settings.setValue(SETTINGS_CUSTOM_CACHE_PATH, m_ui->customCacheEdit->text());
+    settings.setValue(SETTINGS_CACHE_MAX_SIZE, m_ui->cacheMaxSize->value() * CACHE_SCALE);
+
     settings.endGroup();
 
     settings.beginGroup(SETTINGS_PREFERENCES_SECTION);
--- settingsconstants.h
+++ settingsconstants.h
@@ -4,22 +4,41 @@
 #include <QString>
 
 const QString SETTINGS_MAIN_SECTION = "MainWindow";
+
 const QString SETTINGS_GEOMETRY = "geometry";
 const QString SETTINGS_WINDOW_STATE = "window_state";
 const QString SETTINGS_SHOW_STATUSBAR = "show_statusbar";
+
 const QString SETTINGS_AUTOREFRESH_ENABLED = "autorefresh_enabled";
 const QString SETTINGS_AUTOREFRESH_TIMEOUT = "autorefresh_timeout";
 const int     SETTINGS_AUTOREFRESH_TIMEOUT_DEFAULT = 5000; // in miliseconds
+
 const QString SETTINGS_IMAGE_FORMAT = "image_format";
+
 const QString SETTINGS_USE_CUSTOM_JAVA = "use_custom_java";
 const bool    SETTINGS_USE_CUSTOM_JAVA_DEFAULT = true;
 const QString SETTINGS_CUSTOM_JAVA_PATH = "custom_java";
 const QString SETTINGS_CUSTOM_JAVA_PATH_DEFAULT = "/usr/bin/java";
+
 const QString SETTINGS_USE_CUSTOM_PLANTUML = "use_custom_plantuml";
 const bool    SETTINGS_USE_CUSTOM_PLANTUML_DEFAULT = true;
 const QString SETTINGS_CUSTOM_PLANTUML_PATH = "custom_plantuml";
 const QString SETTINGS_CUSTOM_PLANTUML_PATH_DEFAULT = "/usr/bin/plantuml";
+
+const QString SETTINGS_USE_CUSTOM_GRAPHIZ = "use_custom_graphiz";
+const bool    SETTINGS_USE_CUSTOM_GRAPHIZ_DEFAULT = true;
+const QString SETTINGS_CUSTOM_GRAPHIZ_PATH = "custom_graphiz";
+const QString SETTINGS_CUSTOM_GRAPHIZ_PATH_DEFAULT = "/usr/bin/dot";
+
 const QString SETTINGS_ASSISTANT_XML_PATH = "assistant_xml";
+
+const QString SETTINGS_USE_CACHE = "use_cache";
+const bool    SETTINGS_USE_CACHE_DEFAULT = true;
+const QString SETTINGS_USE_CUSTOM_CACHE = "use_custom_cache";
+const bool    SETTINGS_USE_CUSTOM_CACHE_DEFAULT = false;
+const QString SETTINGS_CUSTOM_CACHE_PATH = "custom_cache";
+const QString SETTINGS_CACHE_MAX_SIZE = "cache_max_size";
+const int     SETTINGS_CACHE_MAX_SIZE_DEFAULT = 50000; // in bytes
 
 const QString SETTINGS_RECENT_DOCUMENTS_SECTION = "recent_documents";
 const QString SETTINGS_RECENT_DOCUMENTS_DOCUMENT = "document";
